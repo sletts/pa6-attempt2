c00de3398ad02f90d6815e0961c0929c879a0fb0
sletts <qopster@gmail.com>
2020-11-05 11:13:04 -0800

    Initial Commit

---
+++ Traveler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Traveler
+//
+//  Created by Letts, Sean Aleksey on 11/5/20.
+//  Copyright © 2020 Letts, Sean Aleksey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
